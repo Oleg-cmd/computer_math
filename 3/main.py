fa79b876ba17ebb69555e7641eb31bf7b2f498bb
oleg-cmd <oleg.selanta@gmail.com>
2024-04-11 17:19:08 +0300

    some imporves

--- 3/main.py
+++ 3/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 import math
+import inspect
 
 # Define the variable and function for symbolic integration
 x = sp.symbols("x")
@@ -14,7 +15,7 @@ def check_convergence(func, a, b):
         f = func(x)
         integral_result = sp.integrate(f, (x, a, b))
         # print(integral_result)
-        if integral_result.is_infinite:
+        if True:
             print("Интеграл не существует")
             return False
         else:
@@ -25,7 +26,6 @@ def check_convergence(func, a, b):
         return False
 
 
-# Define a list of functions to choose from
 def function_1(x):
     return 3 * x**3 - 2 * x**2 - 7 * x - 8
 
@@ -39,11 +39,26 @@ def function_3(x):
 
 
 def function_4(x):
+
+    # -inf - 0
     return math.e ** (-x)
 
 
+def function_5(x):
+
+    # 0 - 1
+    return 1 / x
+
+
 def general_transform(func, a, b, method, n):
-    if np.isinf(b):
+    if np.isinf(a) and np.isinf(b):
+        # Замена переменной x = t / (1 - t**2) преобразует (-inf, inf) в (-1, 1)
+        transformed_func = lambda t: np.where(
+            np.abs(t) == 1, 0, func(t / (1 - t**2)) * (1 + t**2) / ((1 - t**2) ** 2)
+        )
+        a_new, b_new = -1, 1
+        result = method(transformed_func, a_new, b_new, n)
+    elif np.isinf(b):
         # Замена переменной x = 1/t - 1 преобразует [0, inf) в [1, 0)
         transformed_func = lambda t: np.where(t == 0, 0, func(1 / t - 1) * (1 / t**2))
         a_new, b_new = 1, 0 if a == 0 else 1 / a
@@ -59,7 +74,7 @@ def general_transform(func, a, b, method, n):
 
 
 # List of functions
-functions = [function_1, function_2, function_3, function_4]
+functions = [function_1, function_2, function_3, function_4, function_5]
 
 
 # Midpoint rule
@@ -98,8 +113,6 @@ def simpsons_rule(func, a, b, n):
 
 
 # Left rectangle rule
-
-
 def left_rectangle_rule(func, a, b, n):
     h = (b - a) / n
     total = 0
@@ -110,8 +123,6 @@ def left_rectangle_rule(func, a, b, n):
 
 
 # Right rectangle rule
-
-
 def right_rectangle_rule(func, a, b, n):
     h = (b - a) / n
     total = 0
@@ -121,7 +132,6 @@ def right_rectangle_rule(func, a, b, n):
     return h * total
 
 
-# Define a mapping from user input to the actual functions
 methods = {
     "left": left_rectangle_rule,
     "right": right_rectangle_rule,
@@ -131,7 +141,14 @@ methods = {
 }
 
 
-# User interaction to choose function and method
+def get_function_string(f):
+    source_lines = inspect.getsource(f)
+    source_lines = source_lines.split("\n")[1:]
+    function_string = "\n".join(source_lines)
+    function_string = function_string.replace("return ", "", 1)
+    return function_string
+
+
 def parse_input(value):
     if value == "inf":
         return float("inf")
@@ -144,14 +161,22 @@ def parse_input(value):
             raise ValueError("Invalid input for limit of integration")
 
 
-func_choice = int(input("Function number (1-4): "))
+print("Welcome")
+print(
+    get_function_string(function_1),
+    get_function_string(function_2),
+    get_function_string(function_3),
+    get_function_string(function_4),
+    get_function_string(function_5),
+)
+
+func_choice = int(input("Function number (1-5): "))
 method_choice = input("Method (left, right, midpoint, trapezoid, simpson): ")
 a = input("Lower limit of integration (type 'inf' for infinity): ")
 b = input("Upper limit of integration (type 'inf' for infinity): ")
 n = int(input("Initial number of subintervals (n): "))
 
 # Handling infinite limits
-
 a = parse_input(a)
 b = parse_input(b)
 
